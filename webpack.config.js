57ee427ea180acc898a1e055b47116293d8d88e1
instakarate <forinstatech@gmail.com>
2025-10-31 21:39:10 +0530

    fix: correct webpack publicPath for GitHub Pages subdirectory

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,30 @@
 // webpack.config.js
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = (env, argv) => ({
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: argv.mode === 'production' ? '[name].[contenthash].js' : '[name].js',
-    publicPath: '',
+    publicPath: '/kiddo/',
     clean: true,
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
-      { test: /\.(png|jpg|gif|wav|mp3)$/, type: 'asset/resource' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|wav|mp3)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
     ],
   },
   plugins: [
@@ -31,10 +42,9 @@ module.exports = (env, argv) => ({
   },
   devtool: argv.mode === 'production' ? 'source-map' : 'eval-source-map',
   optimization: {
-    splitChunks: { chunks: 'all' },
+    splitChunks: {
+      chunks: 'all'
+    },
     minimize: argv.mode === 'production',
-  },
-  resolve: {
-    extensions: ['.js'],
   },
 });
